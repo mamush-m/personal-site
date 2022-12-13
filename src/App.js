220c8400db92618f7314e6464bd198fd6d40a696
mamush-m <svrmamush@gmail.com>
2022-12-13 03:18:04 -0500

    dec edition

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import { Navigation } from './components/navigation/Navigation';
+import { Home } from './components/home/Home';
+import { About } from './components/about/About';
+import { Portfolio } from './components/portfolio/Portfolio';
+import { Contact } from './components/contact/Contact';
+import { useState } from 'react';
+// import { Display } from './components/display/Display';
+// import { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
+
+  const [menu, setMenu] = useState(false);
+
+  // const toggle = () => {
+  //   setMenu(false)
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+     {/* <Navigation content={content} updater={updater}/> */}
+     <Navigation menu={menu} setMenu={setMenu}/>
+
+
+     <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/portfolio' element={<Portfolio/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+     </Routes>
+
+
+     {/* <Home/> */}
+     {/* <Display content={content}/> */}
     </div>
   );
 }
